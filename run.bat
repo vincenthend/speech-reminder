8d56f8565d052643d6c8af8a3604265d9f68a253
Roland Hartanto <rolandhartanto@gmail.com>
2018-11-24 20:12:13 +0700

    generate phone1.mlf

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 HParse gram wdnet
 HDman -l log.txt -n stats.txt dict_used lexicon/dict
 HLEd -A -D -T 1 -l * -d dict_used -i phones0.mlf mkphones0.led words.mlf
+HLEd -A -D -T 1 -l * -d dict_used -i phones1.mlf mkphones1.led words.mlf
