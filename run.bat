4f9b99372917e9c4dc5d7c2c549b1671937d0cf3
vincenthend <vincenthendrha@gmail.com>
2018-11-25 18:51:14 +0700

    Fixed script error

--- run.bat
+++ run.bat
@@ -16,8 +16,8 @@ py scripts/monophones0.py
 
 rem create mlf file containing transcript of wav files, and generate label with led script
 py scripts/train_transcript.py
-HLEd -A -D -T 1 -l * -d generated/dict_used -i generated/phones/phones0.mlf train/mkphones0.led generated/open_train_transcript.mlf.mlf
-HLEd -A -D -T 1 -l * -d generated/dict_used -i generated/phones/phones1.mlf train/mkphones1.led generated/open_train_transcript.mlf.mlf
+HLEd -A -D -T 1 -l * -d generated/dict_used -i generated/phones/phones0.mlf train/mkphones0.led generated/open_train_transcript.mlf
+HLEd -A -D -T 1 -l * -d generated/dict_used -i generated/phones/phones1.mlf train/mkphones1.led generated/open_train_transcript.mlf
 
 py scripts/train_scp.py
 HCopy -A -D -T 1 -C configurations/wav_config -S generated/open_train_codetrain.scp
@@ -35,19 +35,19 @@ mkdir hmms\hmm6
 mkdir hmms\hmm7
 mkdir hmms\hmm8
 mkdir hmms\hmm9
-HERest -A -D -T 1 -C configurations/config -I generated/phones/phones0.mlf -t 250.0 150.0 1000.0 -S generated/train.scp -H hmms/hmm0/macros -H hmms/hmm0/hmmdefs -M hmms/hmm1 generated/monophones0
-HERest -A -D -T 1 -C configurations/config -I generated/phones/phones0.mlf -t 250.0 150.0 1000.0 -S generated/train.scp -H hmms/hmm1/macros -H hmms/hmm1/hmmdefs -M hmms/hmm2 generated/monophones0
-HERest -A -D -T 1 -C configurations/config -I generated/phones/phones0.mlf -t 250.0 150.0 1000.0 -S generated/train.scp -H hmms/hmm2/macros -H hmms/hmm2/hmmdefs -M hmms/hmm3 generated/monophones0
+HERest -A -D -T 1 -C configurations/config -I generated/phones/phones0.mlf -t 250.0 150.0 1000.0 -S generated/open_train_train.scp -H hmms/hmm0/macros -H hmms/hmm0/hmmdefs -M hmms/hmm1 generated/monophones0
+HERest -A -D -T 1 -C configurations/config -I generated/phones/phones0.mlf -t 250.0 150.0 1000.0 -S generated/open_train_train.scp -H hmms/hmm1/macros -H hmms/hmm1/hmmdefs -M hmms/hmm2 generated/monophones0
+HERest -A -D -T 1 -C configurations/config -I generated/phones/phones0.mlf -t 250.0 150.0 1000.0 -S generated/open_train_train.scp -H hmms/hmm2/macros -H hmms/hmm2/hmmdefs -M hmms/hmm3 generated/monophones0
 copy hmms\hmm3\macros hmms\hmm4\macros
 py scripts/sp_modeling.py
 HHEd -A -D -T 1 -H hmms/hmm4/macros -H hmms/hmm4/hmmdefs -M hmms/hmm5 train/sil.hed generated/monophones1
-HERest -A -D -T 1 -C configurations/config  -I generated/phones/phones1.mlf -t 250.0 150.0 3000.0 -S generated/train.scp -H hmms/hmm5/macros -H hmms/hmm5/hmmdefs -M hmms/hmm6 generated/monophones1
-HERest -A -D -T 1 -C configurations/config  -I generated/phones/phones1.mlf -t 250.0 150.0 3000.0 -S generated/train.scp -H hmms/hmm6/macros -H hmms/hmm6/hmmdefs -M hmms/hmm7 generated/monophones1
+HERest -A -D -T 1 -C configurations/config  -I generated/phones/phones1.mlf -t 250.0 150.0 3000.0 -S generated/open_train_train.scp -H hmms/hmm5/macros -H hmms/hmm5/hmmdefs -M hmms/hmm6 generated/monophones1
+HERest -A -D -T 1 -C configurations/config  -I generated/phones/phones1.mlf -t 250.0 150.0 3000.0 -S generated/open_train_train.scp -H hmms/hmm6/macros -H hmms/hmm6/hmmdefs -M hmms/hmm7 generated/monophones1
 
 rem align utterances and reestimate parameter
-HVite -A -D -T 1 -l * -o SWT -b SENT-END -C configurations/config -H hmms/hmm7/macros -H hmms/hmm7/hmmdefs -i generated/aligned.mlf -m -t 250.0 150.0 1000.0 -y lab -a -I generated/words.mlf -S generated/train.scp generated/dict_used generated/monophones1> logs/HVite_log
-HERest -A -D -T 1 -C configurations/config -I generated/aligned.mlf -t 250.0 150.0 3000.0 -S generated/train.scp -H hmms/hmm7/macros -H hmms/hmm7/hmmdefs -M hmms/hmm8 generated/monophones1
-HERest -A -D -T 1 -C configurations/config -I generated/aligned.mlf -t 250.0 150.0 3000.0 -S generated/train.scp -H hmms/hmm8/macros -H hmms/hmm8/hmmdefs -M hmms/hmm9 generated/monophones1
+HVite -A -D -T 1 -l * -o SWT -b SENT-END -C configurations/config -H hmms/hmm7/macros -H hmms/hmm7/hmmdefs -i generated/aligned.mlf -m -t 250.0 150.0 1000.0 -y lab -a -I generated/open_train_transcript.mlf -S generated/open_train_train.scp generated/dict_used generated/monophones1> logs/HVite_log
+HERest -A -D -T 1 -C configurations/config -I generated/aligned.mlf -t 250.0 150.0 3000.0 -S generated/open_train_train.scp -H hmms/hmm7/macros -H hmms/hmm7/hmmdefs -M hmms/hmm8 generated/monophones1
+HERest -A -D -T 1 -C configurations/config -I generated/aligned.mlf -t 250.0 150.0 3000.0 -S generated/open_train_train.scp -H hmms/hmm8/macros -H hmms/hmm8/hmmdefs -M hmms/hmm9 generated/monophones1
 
 rem create list of triphones with HLEd, clone HMM Models with HHEd (using mktri.hed as script), and reestimate parameter of HMM Models with HERest into hmm11
 HLEd -A -D -T 1 -n generated/triphones1 -l * -i generated/wintri.mlf train/mktri.led generated/aligned.mlf
@@ -56,8 +56,8 @@ mkdir hmms\hmm10
 mkdir hmms\hmm11
 mkdir hmms\hmm12
 HHEd -A -D -T 1 -H hmms/hmm9/macros -H hmms/hmm9/hmmdefs -M hmms/hmm10 generated/mktri.hed generated/monophones1 
-HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -S generated/train.scp -H hmms/hmm10/macros -H hmms/hmm10/hmmdefs -M hmms/hmm11 generated/triphones1
-HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -s generated/hmm12stats -S generated/train.scp -H hmms/hmm11/macros -H hmms/hmm11/hmmdefs -M hmms/hmm12 generated/triphones1 
+HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -S generated/open_train_train.scp -H hmms/hmm10/macros -H hmms/hmm10/hmmdefs -M hmms/hmm11 generated/triphones1
+HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -s generated/hmm12stats -S generated/open_train_train.scp -H hmms/hmm11/macros -H hmms/hmm11/hmmdefs -M hmms/hmm12 generated/triphones1 
 
 rem output triphones version of dictionary entry, remove duplicate triphones
 HDMan -A -D -T 1 -b sp -n generated/fulllist0 -g train/maketriphones.ded -l generated/hdmanflog generated/dict-tri train/lexicon/dict
@@ -69,5 +69,5 @@ mkdir hmms\hmm13
 mkdir hmms\hmm14
 mkdir hmms\hmm15
 HHEd -A -D -T 1 -H hmms/hmm12/macros -H hmms/hmm12/hmmdefs -M hmms/hmm13 generated/tree.hed generated/triphones1
-HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -S generated/train.scp -H hmms/hmm13/macros -H hmms/hmm13/hmmdefs -M hmms/hmm14 generated/tiedlist
-HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -S generated/train.scp -H hmms/hmm14/macros -H hmms/hmm14/hmmdefs -M hmms/hmm15 generated/tiedlist
+HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -S generated/open_train_train.scp -H hmms/hmm13/macros -H hmms/hmm13/hmmdefs -M hmms/hmm14 generated/tiedlist
+HERest  -A -D -T 1 -C configurations/config -I generated/wintri.mlf -t 250.0 150.0 3000.0 -S generated/open_train_train.scp -H hmms/hmm14/macros -H hmms/hmm14/hmmdefs -M hmms/hmm15 generated/tiedlist
--- scripts/train_transcript.py
+++ scripts/train_transcript.py
@@ -7,10 +7,19 @@ files.write('#!MLF!#\n')
 for transcript in transcript_arr:
     t = transcript.split(' ')
     files.write('"*/'+t[0]+'lab"\n')
-    for x in t[1:-1]:
-        # if(x != "SENT-START" and x != "SENT-END" and x != ""):
-        a = x.replace("\n", "")
-        a = a.replace("\r", "")
-        files.write(a+"\n")
+    if(len(t) == 2):
+        for x in t[1:]:
+            if(x != ""):
+                a = x.replace("\n", "")
+                a = a.replace("\r", "")
+                files.write("SENT-START\n")
+                files.write(a+"\n")
+                files.write("SENT-END\n")
+    else:
+        for x in t[1:-1]:
+            if(x != ""):
+                a = x.replace("\n", "")
+                a = a.replace("\r", "")
+                files.write(a+"\n")
     files.write('.\n')
 files.close()
